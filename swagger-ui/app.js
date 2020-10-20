d91a55c55a8adb6f2bd6665e8cf9a3e9f04b0b09
Kleyson <kleysondev@gmail.com>
2020-10-20 13:13:42 -0300

    Add Newman to Automatic Tests

--- swagger-ui/app.js
+++ swagger-ui/app.js
@@ -1,25 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const swaggerUi = require('swagger-ui-express');
-const port = 3000;
- 
+const swaggerUi = require("swagger-ui-express");
+const port = 4200;
+
 var options = {
   explorer: true,
   swaggerOptions: {
     urls: [
       {
-        url: 'https://raw.githubusercontent.com/nsccomunicacao/clube-do-assinante-documentation/master/clube-nsc-swagger-app.yaml',
-        name: 'APP'
+        url:
+          "https://raw.githubusercontent.com/nsccomunicacao/clube-do-assinante-documentation/master/clube-nsc-swagger-app.yaml",
+        name: "App",
       },
       {
-        url: 'https://raw.githubusercontent.com/nsccomunicacao/clube-do-assinante-documentation/master/clube-nsc-swagger-site.yaml',
-        name: 'Site'
-      }
-    ]
-  }
-}
- 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(null, options));
+        url:
+          "https://raw.githubusercontent.com/nsccomunicacao/clube-do-assinante-documentation/master/clube-nsc-swagger-site.yaml",
+        name: "Site",
+      },
+      {
+        url:
+          "https://raw.githubusercontent.com/nsccomunicacao/clube-do-assinante-documentation/master/clube-nsc-swagger-backoffice.yaml",
+        name: "Backoffice",
+      },
+    ],
+  },
+};
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(null, options));
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api-docs`)
-})
+  console.log(`Listening at http://localhost:${port}/api-docs`);
+});
